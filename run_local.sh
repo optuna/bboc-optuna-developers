c4aff0d4262b87327bdbf630634df1a1bdb4c02b
c-bata <contact@c-bata.link>
2020-12-09 17:17:42 +0900

    Fix run_local.sh

--- run_local.sh
+++ run_local.sh
@@ -32,10 +32,9 @@ mkdir -p $DB_ROOT
 # Copy the baseline file in, we can skip this but we must include RandomSearch in the -o list
 ! test -d $DB_ROOT/$DBID/  # Check the folder does not yet exist
 bayesmark-init -dir $DB_ROOT -b $DBID
-cp ./input/baseline-$N_STEP-$N_BATCH.json $DB_ROOT/$DBID/derived/baseline.json
 
 # By default, runs on all models (-c), data (-d), metrics (-m)
-bayesmark-launch -dir $DB_ROOT -b $DBID -n $N_STEP -r $N_REPEAT -p $N_BATCH -o $OPT --opt-root $OPT_ROOT -v -c SVM DT -d boston wine
+bayesmark-launch -dir $DB_ROOT -b $DBID -n $N_STEP -r $N_REPEAT -p $N_BATCH -o RandomSearch $OPT --opt-root $OPT_ROOT -v -c SVM DT -d boston wine
 # To run on all problems use instead (slower):
 # bayesmark-launch -dir $DB_ROOT -b $DBID -n $N_STEP -r $N_REPEAT -p $N_BATCH -o $OPT --opt-root $OPT_ROOT -v
 
